ce87764ceaf6787d8b3c040a2fce957ed99f57e5
AndrewSuyer <andrew.suyer@icloud.com>
2025-11-15 11:36:52 -0500

    Initial commit

---
+++ test/shopcomp-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ShopcompBackend from '../lib/shopcomp-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/shopcomp-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ShopcompBackend.ShopcompBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
