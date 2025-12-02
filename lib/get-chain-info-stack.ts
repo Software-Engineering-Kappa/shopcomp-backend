2663d47a916fa36d2c8abade438111ce934ecba4
asuyer-wpi <asuyer@wpi.edu>
2025-12-02 13:21:22 -0500

    Created template getChainInfo

---
+++ lib/get-chain-info-stack.ts
@@ -0,0 +1,58 @@
+import * as cdk from "aws-cdk-lib"
+import { Construct } from "constructs"
+import * as apigw from "aws-cdk-lib/aws-apigateway"
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as lambdaNodejs from "aws-cdk-lib/aws-lambda-nodejs";
+import * as path from "node:path"
+import * as ec2 from "aws-cdk-lib/aws-ec2"
+import { Duration } from "aws-cdk-lib"
+
+// Load environment variables in `.env` file
+import * as dotenv from "dotenv"
+dotenv.config()
+
+interface GetChainInfoStackProps extends cdk.StackProps {
+  apiEndpoint: apigw.RestApi,
+  vpc: ec2.IVpc,
+  securityGroup: ec2.ISecurityGroup,
+  authorizer: apigw.IAuthorizer,
+}
+
+export class GetChainInfoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: GetChainInfoStackProps) {
+    super(scope, id, props)
+
+    // BEGIN: /chains/{chainId}/ endpoint
+
+    const getChainInfoFn = new lambdaNodejs.NodejsFunction(this, "getChainInfo", {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: "getChainInfo.handler",
+      code: lambda.Code.fromAsset(path.join(__dirname, "getChainInfo")),
+      vpc: props!.vpc,
+      securityGroups: [props!.securityGroup],
+      timeout: Duration.seconds(3),
+      environment: {
+        DB_HOST: process.env.DB_HOST!,
+        DB_USER: process.env.DB_USER!,
+        DB_PASSWORD: process.env.DB_PASSWORD!,
+        DB_NAME: process.env.DB_NAME!,
+      }
+    })
+
+    // /chains
+    const chainsResource = props!.apiEndpoint.root.getResource("chains")
+      ?? props!.apiEndpoint.root.addResource("chains")
+
+    // /chains/{chainId}
+    const chainIdResource = chainsResource.getResource("{chainId}")
+      ?? chainsResource.addResource("{chainId}")
+
+    // TODO: Add authorizer
+    chainIdResource.addMethod("GET", new apigw.LambdaIntegration(getChainInfoFn), {
+      // authorizer: props!.authorizer,
+      // authorizationType: apigw.AuthorizationType.COGNITO,
+    })
+
+    // END: /chains/{chainId}/stores endpoint
+  }
+}
