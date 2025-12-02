b642d3686a4ad11bf178b115433faa6eb0f412f0
asuyer-wpi <asuyer@wpi.edu>
2025-12-02 17:37:37 -0500

    Moved optionsHandler to a lambda function folder like the others

--- lib/api-stack.ts
+++ lib/api-stack.ts
@@ -3,6 +3,9 @@ import { Construct } from "constructs"
 import * as apigw from "aws-cdk-lib/aws-apigateway"
 import * as cognito from "aws-cdk-lib/aws-cognito"
 import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as lambdaNodejs from "aws-cdk-lib/aws-lambda-nodejs";
+import * as path from "node:path"
+import { Duration } from "aws-cdk-lib"
 
 interface ApiStackProps extends cdk.StackProps {
   userPool: cognito.UserPool,
@@ -30,7 +33,7 @@ export class ApiStack extends cdk.Stack {
       //   allowMethods: apigw.Cors.ALL_METHODS,
       //   allowHeaders: apigw.Cors.DEFAULT_HEADERS,
       // },
-      defaultCorsPreflightOptions: undefined,
+      // defaultCorsPreflightOptions: undefined,
     })
 
     // Create authorizer for this user pool
@@ -38,34 +41,38 @@ export class ApiStack extends cdk.Stack {
       cognitoUserPools: [props!.userPool]
     })
 
-    const allowedOrigins = [
-      "http://localhost:3000",    // local development
-      "http://localhost:3001",    // local development
-      "http://shop-comp-s3-bucket.s3-website-us-east-1.amazonaws.com"
-    ]
+    // Function to handle all OPTIONS requests for CORS preflight
+    const optionsHandler = new lambdaNodejs.NodejsFunction(this, "optionsHandler", {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: "optionsHandler.handler",
+      code: lambda.Code.fromAsset(path.join(__dirname, "optionsHandler")),
+      timeout: Duration.seconds(3),
+    })
 
     // Function to handle all OPTIONS requests for CORS preflight
-    const optionsHandler = new lambda.Function(this, "optionsHandler", {
-      runtime: lambda.Runtime.NODEJS_22_X,
-      handler: "index.handler",
-      code: lambda.Code.fromInline(`
-        exports.handler = async (event) => {
-          const origin = event.headers?.origin;
-          const allowed = ${JSON.stringify(allowedOrigins)};
-          const allowOrigin = allowed.includes(origin) ? origin : allowed[0];
-
-          return {
-            statusCode: 200,
-            headers: {
-              "Access-Control-Allow-Origin": allowOrigin,
-              "Access-Control-Allow-Credentials": "true",
-              "Access-Control-Allow-Headers": "Content-Type,Authorization",
-              "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-            }
-          };
-        };
-      `),
-    });
+    // const optionsHandler = new lambda.Function(this, "optionsHandler", {
+    //   runtime: lambda.Runtime.NODEJS_22_X,
+    //   handler: "index.handler",
+    //   code: lambda.Code.fromInline(`
+    //     exports.handler = async (event) => {
+    //       const origin = event.headers.origin || event/headers.Origin
+    //       const allowed = ${JSON.stringify(allowedOrigins)}
+    //       const allowOrigin = allowed.includes(origin) ? origin : allowed[0]
+    //       console.log("Allowing origin: ", allowOrigin)
+    //
+    //       return {
+    //         statusCode: 200,
+    //         headers: {
+    //           "Access-Control-Allow-Origin": allowOrigin,
+    //           "Access-Control-Allow-Credentials": "true",
+    //           "Access-Control-Allow-Headers": "Content-Type,Authorization,X-Api-Key,X-Amz-Date,X-Amz-Security-Token,Accept,Origin,User-Agent",
+    //           "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+    //         },
+    //         body: "",
+    //       }
+    //     }
+    //   `),
+    // });
 
     // Make optionsHandler handle OPTIONS for all paths
     this.apiEndpoint.root
@@ -76,22 +83,20 @@ export class ApiStack extends cdk.Stack {
     this.apiEndpoint.root.addMethod("OPTIONS", new apigw.LambdaIntegration(optionsHandler))
 
     // Make error responses still have CORS headers
-    this.apiEndpoint.addGatewayResponse("cors4xx", {
-      type: apigw.ResponseType.DEFAULT_4XX,
-      responseHeaders: {
-        "Access-Control-Allow-Origin": "method.request.header.origin",
-        "Access-Control-Allow-Credentials": "'true'",
-      },
-    });
-
-    this.apiEndpoint.addGatewayResponse("cors5xx", {
-      type: apigw.ResponseType.DEFAULT_5XX,
-      responseHeaders: {
-        "Access-Control-Allow-Origin": "method.request.header.origin",
-        "Access-Control-Allow-Credentials": "'true'",
-      },
-    });
-
-
+    // this.apiEndpoint.addGatewayResponse("cors4xx", {
+    //   type: apigw.ResponseType.DEFAULT_4XX,
+    //   responseHeaders: {
+    //     "Access-Control-Allow-Origin": "method.request.header.origin",
+    //     "Access-Control-Allow-Credentials": "'true'",
+    //   },
+    // });
+    //
+    // this.apiEndpoint.addGatewayResponse("cors5xx", {
+    //   type: apigw.ResponseType.DEFAULT_5XX,
+    //   responseHeaders: {
+    //     "Access-Control-Allow-Origin": "method.request.header.origin",
+    //     "Access-Control-Allow-Credentials": "'true'",
+    //   },
+    // });
   }
 }
