3ea73b2811f45d68ad81e9504149c8b834b69ac0
asuyer-wpi <asuyer@wpi.edu>
2025-12-02 14:09:24 -0500

    Created getChainInfo lambda on its own stack. Added stack to the application.

--- bin/shopcomp-backend.ts
+++ bin/shopcomp-backend.ts
@@ -8,6 +8,7 @@ import { AuthorizationStack } from '../lib/authorization-stack'
 import { ApiStack } from "../lib/api-stack"
 import { VpcStack } from "../lib/vpc-stack"
 import { ConfirmShopperStack } from "../lib/confirm-shopper-stack"
+import { GetChainInfoStack } from "../lib/get-chain-info-stack"
 import { ListChainsStack } from "../lib/list-chains-stack"
 import { ListStoresStack } from "../lib/list-stores-stack"
 import { LoginShopperStack } from "../lib/login-shopper-stack"
@@ -61,6 +62,13 @@ new ConfirmShopperStack(app, "ConfirmShopperStack", {
   apiEndpoint: apiStack.apiEndpoint,
   vpc: vpcStack.vpc,
   securityGroup: vpcStack.securityGroup,
+})
+
+new GetChainInfoStack(app, "GetChainInfoStack", {
+  apiEndpoint: apiStack.apiEndpoint,
+  vpc: vpcStack.vpc,
+  securityGroup: vpcStack.securityGroup,
+  authorizer: apiStack.authorizer,
 })
 
 new ListChainsStack(app, "ListChainsStack", {
